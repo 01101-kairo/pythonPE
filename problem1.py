27b47838dc2acae33ba2fc40768168a10e96e300
kairo <kairomilhomem@gmail.com>
2021-12-19 12:31:03 -0300

    Primeiro problema

---
+++ problem1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    @name: problema1
+    @dave: Kairo
+    @email: kosta.kairo@gmail.com
+    @github: https://github.com/01101-kairo/
+    @creation: 19/12/24 12h26
+    @change: 19/12/24 12h
+    @Description: Se listarmos todos os números naturais abaixo de 10 que
+    são múltiplos de 3 ou 5, obtemos 3, 5, 6 e 9. A soma desses múltiplos
+    é 23.
+    Encontre a soma de todos os múltiplos de 3 ou 5 abaixo de 1000.
+
+    https://projecteuler.net/problem=1
+"""
