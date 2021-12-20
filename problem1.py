aa36261cd1f0c2781796f394f03b7a285f1d3d34
kairo costa <57676304+01101-kairo@users.noreply.github.com>
2021-12-20 14:04:15 -0300

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -8,10 +8,25 @@
     @github: https://github.com/01101-kairo/
     @creation: 19/12/24 12h26
     @change: 19/12/24 12h
+"""
+
+
+def multiplo(cond):
+    """
     @Description: Se listarmos todos os números naturais abaixo de 10 que
     são múltiplos de 3 ou 5, obtemos 3, 5, 6 e 9. A soma desses múltiplos
     é 23.
     Encontre a soma de todos os múltiplos de 3 ou 5 abaixo de 1000.
 
     https://projecteuler.net/problem=1
-"""
+    """
+    cont = 3
+    soma = 0
+    while cont < cond:
+        if cont % 3 == 0 or cont % 5 == 0:
+            soma = cont+soma
+        cont = cont + 1
+    return soma
+
+
+print(multiplo(1000))
